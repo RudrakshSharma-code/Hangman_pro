e2955fea8f8fe42e70b1a8bf0f3ffd12296d6089
Thi Hong Phuc Le <plth1998@gmail.com>
2020-03-19 23:57:12 -0700

    firstcommit

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+let wordBank =[
+    {
+        word: "ENTITY",
+        definition: "A person, place, thing, or event for which data is collected and maintained"
+    },
+
+    {
+        word: "SCHEMA",
+        definition: "Descriptions of all fields, tables and relationship"
+    },
+
+    {
+        word: "FIELD",
+        definition: "(An attribute) Single characteristic or fact about an entity"
+    },
+
+    {
+        word: "SCALABILITY",
+        definition: "System can be modified, or downsized"
+    }
+
+    // {
+    //     word: "" ,
+    //     definition: ""
+    // }
+]
