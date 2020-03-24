86451403db61dd3cb4f7b7f1dfbe10a26eb2916f
Thi Hong Phuc Le <plth1998@gmail.com>
2020-03-23 21:56:59 -0700

    add 2 functions to create buttonsand modify getQuest()

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
+const MAX = 26;
+const NUM = 65;
+let question;
 let wordBank =[
     {
         word: "ENTITY",
@@ -17,20 +20,44 @@ let wordBank =[
     {
         word: "SCALABILITY",
         definition: "System can be modified, or downsized"
-    }
+    },
 
+    {
+        word: "LET",
+        definition: "A beautiful variable in Javascript"
+    }
     // {
     //     word: "" ,
     //     definition: ""
     // }
-]
+];
+
 //invoke function
 getQuest();
+generateButtons();
 
 // get random word from wordBank
 
 function getQuest(){
     let ranNum = Math.floor(Math.random() * wordBank.length);
     document.getElementById("question").innerHTML = wordBank[ranNum].definition;
+    question = wordBank[ranNum].word; 
+}
+
+//create buttons
+function createButton(a){
+    var but = document.createElement("button");
+    let ch = String.fromCharCode(a);
+    
+    but.innerHTML= ch;
+    but.value = ch;
+    document.body.appendChild(but);
+}
+
+//generate creating buttons
+function generateButtons(){
+    for(let i=0; i < MAX; i++ ){
+        createButton(NUM+i);
+    }
 }
 
